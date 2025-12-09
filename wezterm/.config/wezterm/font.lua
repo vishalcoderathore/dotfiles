5569b3f7a6900b4ca7bdca47cad5a9173ea6bc6e
Vishal Rathore <vishal.rathore87@gmail.com>
2025-12-09 13:52:54 -0600

    Updated Wezterm to Catppuccin theme

--- wezterm/.config/wezterm/font.lua
+++ wezterm/.config/wezterm/font.lua
@@ -4,8 +4,7 @@ return function(config)
     config.font = wezterm.font_with_fallback {
         { family = 'JetBrainsMono Nerd Font', harfbuzz_features = { 'calt', 'liga' } },
     }
-    config.font_size = 11.0
-    config.line_height = 1.2
-    config.dpi = 144
+    config.font_size = 10.5
+    config.line_height = 1.1
     config.adjust_window_size_when_changing_font_size = false
 end
--- wezterm/.config/wezterm/theme_dark.lua
+++ wezterm/.config/wezterm/theme_dark.lua
@@ -1,17 +1,30 @@
--- theme_dark.lua
+-- theme_dark.lua - Catppuccin Macchiato (matching Terminator config)
 return {
-    foreground = '#d9d7ce',
-    background = '#1D1E27',
-    cursor_bg = '#ffcc66',
-    cursor_border = '#ffcc66',
-    selection_bg = '#cccccc',
-    selection_fg = '#000000',
+    foreground = '#CAD3F5',
+    background = '#24273A',
+    cursor_bg = '#F4DBD6',
+    cursor_border = '#F4DBD6',
+    cursor_fg = '#24273A',
+    selection_bg = '#5B6078',
+    selection_fg = '#CAD3F5',
     ansi = {
-        '#191e2a', '#ed8274', '#a6cc70', '#fad07b',
-        '#6dcbfa', '#cfbafa', '#90e1c6', '#c7c7c7',
+        '#494D64', -- black
+        '#ED8796', -- red
+        '#A6DA95', -- green
+        '#EED49F', -- yellow
+        '#8AADF4', -- blue
+        '#F5BDE6', -- magenta
+        '#8BD5CA', -- cyan
+        '#B8C0E0', -- white
     },
     brights = {
-        '#686868', '#f28779', '#bae67e', '#ffd580',
-        '#73d0ff', '#d4bfff', '#95e6cb', '#ffffff',
+        '#5B6078', -- bright black
+        '#ED8796', -- bright red
+        '#A6DA95', -- bright green
+        '#EED49F', -- bright yellow
+        '#8AADF4', -- bright blue
+        '#F5BDE6', -- bright magenta
+        '#8BD5CA', -- bright cyan
+        '#A5ADCB', -- bright white
     },
 }
