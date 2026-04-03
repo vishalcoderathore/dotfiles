b81bb3bb7fe2d1883be88097495c6b015f288982
Vishal Rathore <vishal.rathore87@gmail.com>
2026-04-03 15:41:56 -0500

    Switch Neovim theme from OneDark to Catppuccin Mocha

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,41 +1,35 @@
 return {
-  "navarasu/onedark.nvim", -- Make sure this line exists in your Lazy.nvim plugin list
-  name = "onedark",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
   config = function()
-    require("onedark").setup {
-      style = "cool",
-      transparent = false,
+    require("catppuccin").setup {
+      flavour = "mocha",
+      transparent_background = false,
       term_colors = true,
-      ending_tildes = false,
-      cmp_itemkind_reverse = false,
-
-      toggle_style_key = nil,
-      toggle_style_list = {"dark", "darker", "cool", "deep", "warm", "warmer", "light"},
-
-      code_style = {
-        comments = "italic",
-        keywords = "none",
-        functions = "none",
-        strings = "none",
-        variables = "none",
+      styles = {
+        comments = { "italic" },
+        keywords = {},
+        functions = {},
+        strings = {},
+        variables = {},
       },
-
-      lualine = {
-        transparent = false,
-      },
-
-      colors = {},
-      highlights = {},
-
-      diagnostics = {
-        darker = true,
-        undercurl = true,
-        background = true,
+      integrations = {
+        treesitter = true,
+        nvimtree = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
       },
     }
 
-    -- Load the colorscheme
-    require("onedark").load()
+    vim.cmd.colorscheme "catppuccin"
   end,
 }
 
